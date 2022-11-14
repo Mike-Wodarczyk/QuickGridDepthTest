472595e49ffef7778cdbdc2d08f48e5336ff5a44
Mike Wodarczyk <mikew@drbronner.com>
2022-11-14 09:38:25 -0800

    Add project files.

---
+++ QuickGridSample/Client/Pages/Filter.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace QuickGridSample.Client.Pages
+{
+	public class Filter
+	{
+
+		public Action? OnChange { get; set; }
+
+		private string _filter = "";
+		private Regex? filterRegex = null;
+
+		public bool IsMatch(string value)
+		{
+			return filterRegex?.IsMatch(value) ?? true;
+		}
+
+		public bool HasPattern { get => filterRegex != null; }
+		public string Pattern
+		{
+			get => _filter;
+			set
+			{
+				if (_filter != value)
+				{
+					_filter = value;
+					if (string.IsNullOrEmpty(_filter))
+					{
+						filterRegex = null;
+					}
+					else
+					{
+						filterRegex = new Regex(_filter, RegexOptions.IgnoreCase);
+					}
+					OnChange?.Invoke();
+				}
+			}
+		}
+	}
+}
